024812cb77c1c770fad09d0c98b4e8593277c61f
Juan <Juan.murilloper@cun.edu.co>
2026-09-15 00:35:58 -0500

    Cambios html

---
+++ script.js
@@ -0,0 +1,83 @@
+
+// let nombre = " juansho ";
+
+// // alert(saludos + nombre + apellido);
+// let peso_tierra = 70;
+//  let peso_mercurio = peso_tierra * 0.38;
+// let peso_venus = peso_tierra * 0.91;
+//  let peso_marte = peso_tierra * 0.38;
+// let peso_jupiter = peso_tierra * 2.53; let peso_saturno = peso_tierra * 1.06;
+// let peso_urano = peso_tierra * 0.89;
+// let peso_neptuno = peso_tierra * 1.14;
+//  console.log("el peso de" + nombre + " en tierra es " + peso_mercurio + "kg" );
+// console.log("el peso de" + nombre + " en tierra es " + peso_venus + "kg" );
+//  console.log("el peso de" + nombre + " en tierra es " + peso_marte + "kg" );
+//  console.log("el peso de" + nombre + " en tierra es " + peso_jupiter + "kg" );
+//  console.log("el peso de" + nombre + " en tierra es " + peso_saturno + "kg" );
+//  console.log("el peso de" + nombre + " en tierra es " + peso_urano + "kg" );
+//  console.log("el peso de" + nombre + " en tierra es " + peso_neptuno + "kg" );
+
+// let peso_escogido = peso_saturno ;
+
+// if (peso_escogido === peso_jupiter) {
+//     console.log("el peso escogido es: " + peso_jupiter + " kg");
+//  } else if (peso_escogido === peso_marte) {
+//      console.log("el peso escogido es: " + peso_marte + " kg");
+//  } else if (peso_escogido === peso_mercurio) {
+//     console.log("el peso escogido es: " + peso_mercurio + " kg");
+// }
+//  else if (peso_escogido === peso_venus) {
+//     console.log("el peso escogido es: " + peso_venus + " kg");
+//  }
+//  else if (peso_escogido === peso_saturno) {
+//    console.log("el peso escogido es: " + peso_saturno + " kg");
+//  }
+//  else if (peso_escogido === peso_urano) {
+//      console.log("el peso escogido es: " + peso_urano + " kg");
+// }
+//  else if (peso_escogido === peso_neptuno) {
+//      console.log("el peso escogido es: " + peso_neptuno + "kg");
+//  }
+// else {
+//     console.log("el planeta escogido no es valido");
+//  }
+
+ let Temperatura = 25; 
+
+if (Temperatura < 10) {
+     console.log("hace mucho frio");
+  }
+  else if (Temperatura >= 10 && Temperatura < 20) {
+    console.log("hace frio");
+ }
+ else if (Temperatura >= 21 && Temperatura < 30) {
+    console.log("La tempetura es agradable");
+ }
+ else if (Temperatura >= 30 && Temperatura < 40) {
+     console.log("hace mucho calor"); }
+  else {
+     console.log("La temperatura es muy alta");
+ }
+
+
+//   let amor_escogido = "no";
+
+//  if (amor_escogido == "si") {
+//      console.log("el amor es verdadero");
+//  }
+//  else if (amor_escogido == "no") {
+//      console.log("el amor es falso");
+//  }
+
+// let numero_escogido= "10"
+
+// if(numero_escogido == "4" ) {
+//    console.log( " Numero incorrecto " )
+// }
+// else if(numero_escogido == "2") {
+//    console.log( " numero correcto " )
+// }
+// else {
+//    console.log( "numero no existe")
+// }
+
